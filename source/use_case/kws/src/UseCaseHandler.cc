026cec251b4496a621983a6740b86056d4d3b09f
Teppo Jarvelin <teppo.jarvelin@alifsemi.com>
2022-06-16 15:37:21 +0300

    Samples are now slided continously

--- source/use_case/kws/src/UseCaseHandler.cc
+++ source/use_case/kws/src/UseCaseHandler.cc
@@ -34,13 +34,9 @@
 extern uint32_t m55_comms_handle;
 m55_data_payload_t mhu_data;
 
-
 using KwsClassifier = arm::app::Classifier;
 
-#define RECORD_AUDIO
-
-#ifdef RECORD_AUDIO
-volatile uint8_t data_received;
+volatile uint8_t data_received = false;
 
 void audio_callback(uint32_t /*event*/)
 {
@@ -48,14 +44,30 @@ void audio_callback(uint32_t /*event*/)
 }
 
 #define AUDIO_SAMPLES (32000) // 16k samples/sec, 1sec sample, stereo
+#define NEW_SAMPLE_START 8000 // 0.5 second
 
 static int16_t audio_rec[AUDIO_SAMPLES] IFM_BUF_ATTRIBUTE;
+static int16_t audio_inf[AUDIO_SAMPLES] IFM_BUF_ATTRIBUTE;
+static int16_t audio_in[AUDIO_SAMPLES] IFM_BUF_ATTRIBUTE;
 static int16_t audio_max;
 static int16_t audio_min;
 static int16_t audio_average;
 
 namespace arm {
 namespace app {
+
+std::vector<kws::KwsResult> finalResults;
+std::vector<ClassificationResult> singleInfResult;
+/* We expect to be sampling 1 second worth of data at a time.
+*  NOTE: This is only used for time stamp calculation. */
+const float secondsPerSample = 1.0 / audio::MicroNetKwsMFCC::ms_defaultSamplingFreq;
+
+ /**
+ * @brief           Presents KWS inference results.
+ * @param[in]       results     Vector of KWS classification results to be displayed.
+ * @return          true if successful, false otherwise.
+ **/
+static bool PresentInferenceResult(const std::vector<kws::KwsResult>& results);
 
 static void send_msg_if_needed(std::vector<kws::KwsResult> &results)
 {
@@ -72,11 +84,13 @@ static void send_msg_if_needed(std::vector<kws::KwsResult> &results)
                     info("******************* send_msg_if_needed, FOUND GO, copy data end send! ******************\n");
                     strcpy(mhu_data.msg, result.m_resultVec[j].m_label.c_str());
                     SERVICES_send_msg(m55_comms_handle, &mhu_data);
+                    return; // don't send it twice if it was recongnized twive
 
                 } else if (result.m_resultVec[j].m_label == "stop") {
                     info("******************* send_msg_if_needed, FOUND STOP, copy data end send! ******************\n");
                     strcpy(mhu_data.msg, result.m_resultVec[j].m_label.c_str());
                     SERVICES_send_msg(m55_comms_handle, &mhu_data);
+                    return; // don't send it twice if it was recongnized twive
                 }
             }
         }
@@ -99,76 +113,110 @@ void calc_audio_stats(const int16_t* audio, size_t samples)
     audio_average = total / (samples);
 }
 
-int record_audio(int16_t* audio_buffer, size_t samples)
+bool process_audio(ApplicationContext& ctx)
 {
-    hal_set_audio_callback(audio_callback);
-
-    info("Recording audio...\n");
-    int err = hal_get_audio_data(audio_buffer, samples);
-    while (!data_received);
-    data_received = false;
-
-    // Convert sample to mono, after this only samples/2 of data is valid in the buffer
-    for (unsigned int i = 0; i < samples/2; i++) {
-        audio_buffer[i] = (audio_buffer[i*2] >> 1) + (audio_buffer[i*2+1] >> 1);
+    // Convert sample to mono, after this only AUDIO_SAMPLES/2 of data is valid in the buffer
+    for (unsigned int i = 0; i < AUDIO_SAMPLES/2; i++) {
+        audio_in[i] = (audio_in[i*2] >> 1) + (audio_in[i*2+1] >> 1);
     }
-    calc_audio_stats(audio_buffer, samples/2);
+    calc_audio_stats(audio_in, AUDIO_SAMPLES/2);
     printf("Original sample stats: min = %d, max = %d, average = %d\n", audio_min, audio_max, audio_average);
 
-#if 1
     // Normalize and add some gain
-    for (unsigned int i = 0; i < samples/2; i++) {
-        audio_buffer[i] -= audio_average;
-        audio_buffer[i] <<= 2;
+    for (unsigned int i = 0; i < AUDIO_SAMPLES/2; i++) {
+        audio_in[i] -= audio_average;
+        audio_in[i] <<= 2;
     }
-#endif
+
     __DSB();
-    calc_audio_stats(audio_buffer, samples/2);
+    calc_audio_stats(audio_in, AUDIO_SAMPLES/2);
     printf("Normalized sample stats: min = %d, max = %d, average = %d\n", audio_min, audio_max, audio_average);
 
-#if 0
-    // Print out audio sample
-    const int8_t *p = (const int8_t*)audio_buffer;
-    for (unsigned int i = 0; i < samples; i++) {
-        printf("%02hhX ", p[i]);
-        if (!((i+1) % 48)) {
-            printf("\n");
+
+    auto& profiler = ctx.Get<Profiler&>("profiler");
+    auto& model = ctx.Get<Model&>("model");
+    auto& audioDataSlider = ctx.Get<audio::SlidingWindow<const int16_t>&>("swindow");
+    auto& preProcess = ctx.Get<KwsPreProcess&>("preproc");
+    auto& postProcess = ctx.Get<KwsPostProcess&>("postproc");
+    const auto scoreThreshold = ctx.Get<float>("scoreThreshold");
+
+    /*
+    [old 0.5s][new 0.5s][new 0.5s]  // 1st iteration
+    [new 0.5s][new 0.5s][xxxx]      // 2nd iteration
+    [new 0.5s][xxxx][xxxx]          // after 2nd iteration, ready for next call for this function
+    */
+
+    // copy new data after 0.5 of old data
+    memcpy(audio_inf + NEW_SAMPLE_START, audio_in, sizeof(audio_in)/2);
+
+    for (int x = 0; x < 2; x++) {
+
+        audioDataSlider.Reset(audio_inf);
+
+        /* Start sliding through audio clip. */
+        while (audioDataSlider.HasNext()) {
+            const int16_t* inferenceWindow = audioDataSlider.Next();
+
+            /* The first window does not have cache ready. */
+            preProcess.m_audioWindowIndex = audioDataSlider.Index();
+
+            /* Run the pre-processing, inference and post-processing. */
+            if (!preProcess.DoPreProcess(inferenceWindow, audio::MicroNetKwsMFCC::ms_defaultSamplingFreq)) {
+                printf_err("Pre-processing failed.");
+                return false;
+            }
+
+            if (!RunInference(model, profiler)) {
+                printf_err("Inference failed.");
+                return false;
+            }
+
+            if (!postProcess.DoPostProcess()) {
+                printf_err("Post-processing failed.");
+                return false;
+            }
+
+            /* Add results from this window to our final results vector. */
+            finalResults.emplace_back(kws::KwsResult(singleInfResult,
+                    audioDataSlider.Index() * secondsPerSample * preProcess.m_audioDataStride,
+                    audioDataSlider.Index(), scoreThreshold));
+
+#if VERIFY_TEST_OUTPUT
+            DumpTensor(outputTensor);
+#endif /* VERIFY_TEST_OUTPUT */
+        } /* while (audioDataSlider.HasNext()) */
+
+        if (x == 0) {
+            // move 1.0s new data to beginning
+            memmove(audio_inf, audio_inf + NEW_SAMPLE_START, sizeof(audio_inf)/2);
+        } else {
+            // move 0.5s new data to the left so now there is new data in 0 - 7999 waiting for next call to this function and this is then the old data
+            memmove(audio_inf, audio_inf + NEW_SAMPLE_START, NEW_SAMPLE_START*sizeof(audio_inf[0]));
         }
     }
-    printf("\n");
-#endif
-    return err;
+
+    if (!PresentInferenceResult(finalResults)) {
+        return false;
+    }
+
+    profiler.PrintProfilingResult();
+
+    send_msg_if_needed(finalResults);
+
+    finalResults.clear();
+    singleInfResult.clear();
+
+    return true;
 }
-#endif // #ifdef RECORD_AUDIO
 
-
-
-    /**
-     * @brief           Presents KWS inference results.
-     * @param[in]       results     Vector of KWS classification results to be displayed.
-     * @return          true if successful, false otherwise.
-     **/
-    static bool PresentInferenceResult(const std::vector<kws::KwsResult>& results);
 
     /* KWS inference handler. */
-    bool ClassifyAudioHandler(ApplicationContext& ctx, uint32_t clipIndex, bool runAll)
+    bool ClassifyAudioHandler(ApplicationContext& ctx, uint32_t /*clipIndex*/, bool /*runAll*/)
     {
-        auto& profiler = ctx.Get<Profiler&>("profiler");
         auto& model = ctx.Get<Model&>("model");
         const auto mfccFrameLength = ctx.Get<int>("frameLength");
         const auto mfccFrameStride = ctx.Get<int>("frameStride");
-        const auto scoreThreshold = ctx.Get<float>("scoreThreshold");
 
-        /* If the request has a valid size, set the audio index. */
-        if (clipIndex < NUMBER_OF_FILES) {
-            if (!SetAppCtxIfmIdx(ctx, clipIndex,"clipIndex")) {
-                return false;
-            }
-        }
-        auto initialClipIdx = ctx.Get<uint32_t>("clipIndex");
-
-        constexpr uint32_t dataPsnTxtInfStartX = 20;
-        constexpr uint32_t dataPsnTxtInfStartY = 40;
         constexpr int minTensorDims = static_cast<int>(
             (MicroNetKwsModel::ms_inputRowsIdx > MicroNetKwsModel::ms_inputColsIdx)?
              MicroNetKwsModel::ms_inputRowsIdx : MicroNetKwsModel::ms_inputColsIdx);
@@ -194,114 +242,40 @@ int record_audio(int16_t* audio_buffer, size_t samples)
         const uint32_t numMfccFeatures = inputShape->data[MicroNetKwsModel::ms_inputColsIdx];
         const uint32_t numMfccFrames = inputShape->data[arm::app::MicroNetKwsModel::ms_inputRowsIdx];
 
-        /* We expect to be sampling 1 second worth of data at a time.
-         * NOTE: This is only used for time stamp calculation. */
-        const float secondsPerSample = 1.0 / audio::MicroNetKwsMFCC::ms_defaultSamplingFreq;
-
         /* Set up pre and post-processing. */
         KwsPreProcess preProcess = KwsPreProcess(inputTensor, numMfccFeatures, numMfccFrames,
                                                  mfccFrameLength, mfccFrameStride);
 
-        std::vector<ClassificationResult> singleInfResult;
         KwsPostProcess postProcess = KwsPostProcess(outputTensor, ctx.Get<KwsClassifier &>("classifier"),
                                                     ctx.Get<std::vector<std::string>&>("labels"),
                                                     singleInfResult);
 
-        /* Loop to process audio clips. */
-        do {
-            hal_lcd_clear(COLOR_BLACK);
-
-#ifdef RECORD_AUDIO
-            record_audio(audio_rec, AUDIO_SAMPLES);
-
-            auto currentIndex = 0; // ctx.Get<uint32_t>("clipIndex");
-#else
-            auto currentIndex = ctx.Get<uint32_t>("clipIndex");
-#endif
-
-            /* Creating a sliding window through the whole audio clip. */
-            auto audioDataSlider = audio::SlidingWindow<const int16_t>(
-#ifdef RECORD_AUDIO
-                    audio_rec,
+        auto audioDataSlider = audio::SlidingWindow<const int16_t>(
+                    audio_inf,
                     AUDIO_SAMPLES/2,
-#else
-                    get_audio_array(currentIndex),
-                    get_audio_array_size(currentIndex),
-#endif
                     preProcess.m_audioDataWindowSize, preProcess.m_audioDataStride);
 
-            /* Declare a container to hold results from across the whole audio clip. */
-            std::vector<kws::KwsResult> finalResults;
+        ctx.Set<audio::SlidingWindow<const int16_t>&>("swindow", audioDataSlider);
+        ctx.Set<KwsPreProcess&>("preproc", preProcess);
+        ctx.Set<KwsPostProcess&>("postproc", postProcess);
 
-            /* Display message on the LCD - inference running. */
-            std::string str_inf{"Running inference... "};
-            hal_lcd_display_text(str_inf.c_str(), str_inf.size(),
-                    dataPsnTxtInfStartX, dataPsnTxtInfStartY, 0);
-#ifndef RECORD_AUDIO
-            info("Running inference on audio clip %" PRIu32 " => %s\n", currentIndex,
-                 get_filename(currentIndex));
-#endif
+        hal_set_audio_callback(audio_callback);
 
-            /* Start sliding through audio clip. */
-            while (audioDataSlider.HasNext()) {
-                const int16_t* inferenceWindow = audioDataSlider.Next();
-
-                /* The first window does not have cache ready. */
-                preProcess.m_audioWindowIndex = audioDataSlider.Index();
-
-                info("Inference %zu/%zu\n", audioDataSlider.Index() + 1,
-                     audioDataSlider.TotalStrides() + 1);
-
-                /* Run the pre-processing, inference and post-processing. */
-                if (!preProcess.DoPreProcess(inferenceWindow, audio::MicroNetKwsMFCC::ms_defaultSamplingFreq)) {
-                    printf_err("Pre-processing failed.");
+        // sample rate is 16Khz and we support only stereo so AUDIO_ARRAY_SIZE is 32kHz to get 1s audio samples
+        auto err = hal_get_audio_data(audio_rec, AUDIO_SAMPLES);
+        while (err == 0) {
+            if (data_received) {
+                // start receiving new audio and process just received audio stream
+                data_received = false;
+                memcpy(audio_in, audio_rec, sizeof(audio_rec));
+                err = hal_get_audio_data(audio_rec, AUDIO_SAMPLES);
+                if (process_audio(ctx) == false) {
+                    printf_err("process_audio failed\n");
                     return false;
                 }
-
-                if (!RunInference(model, profiler)) {
-                    printf_err("Inference failed.");
-                    return false;
-                }
-
-                if (!postProcess.DoPostProcess()) {
-                    printf_err("Post-processing failed.");
-                    return false;
-                }
-
-                /* Add results from this window to our final results vector. */
-                finalResults.emplace_back(kws::KwsResult(singleInfResult,
-                        audioDataSlider.Index() * secondsPerSample * preProcess.m_audioDataStride,
-                        audioDataSlider.Index(), scoreThreshold));
-
-#if VERIFY_TEST_OUTPUT
-                DumpTensor(outputTensor);
-#endif /* VERIFY_TEST_OUTPUT */
-            } /* while (audioDataSlider.HasNext()) */
-
-            /* Erase. */
-            str_inf = std::string(str_inf.size(), ' ');
-            hal_lcd_display_text(str_inf.c_str(), str_inf.size(),
-                    dataPsnTxtInfStartX, dataPsnTxtInfStartY, false);
-
-            ctx.Set<std::vector<kws::KwsResult>>("results", finalResults);
-
-            if (!PresentInferenceResult(finalResults)) {
-                return false;
             }
-
-            profiler.PrintProfilingResult();
-
-            // send the message to HP is correct word was recognized
-            send_msg_if_needed(finalResults);
-
-            IncrementAppCtxIfmIdx(ctx,"clipIndex");
-
-#ifdef RECORD_AUDIO
-        } while (1);
-#else
-        } while (runAll && ctx.Get<uint32_t>("clipIndex") != initialClipIdx);
-#endif
-        return true;
+        }
+        return false;
     }
 
     static bool PresentInferenceResult(const std::vector<kws::KwsResult>& results)
