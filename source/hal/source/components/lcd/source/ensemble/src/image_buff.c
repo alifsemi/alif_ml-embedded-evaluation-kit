e29273c2a22dca8a24d1cf7d023b344f23e7c8db
Kevin Bracey <kevin.bracey@alifsemi.com>
2022-10-31 17:37:42 +0200

    Make camera buffers zero-init
    Avoids some decompressor glitch in armclang

--- source/hal/source/components/lcd/source/ensemble/src/image_buff.c
+++ source/hal/source/components/lcd/source/ensemble/src/image_buff.c
@@ -45,8 +45,8 @@ ml_image_t act_image = {// __attribute__((section("activation_image")));					// 
 };
 */
 
-uint8_t  	rgb_image[CIMAGE_X*CIMAGE_Y*RGB_BYTES] __attribute__((section("camera_frame_bayer_to_rgb_buf")));	// 560x560x3 = 940,800
-uint8_t  	raw_image[CIMAGE_X*CIMAGE_Y*RGB_BYTES + 0x460] __attribute__((aligned(32),section("camera_frame_buf")));				// 560x560x3 = 940,800
+uint8_t  	rgb_image[CIMAGE_X*CIMAGE_Y*RGB_BYTES] __attribute__((section(".bss.camera_frame_bayer_to_rgb_buf")));		// 560x560x3 = 940,800
+uint8_t  	raw_image[CIMAGE_X*CIMAGE_Y*RGB_BYTES + 0x460] __attribute__((aligned(32),section(".bss.camera_frame_buf")));	// 560x560x3 = 940,800
 
-uint8_t 	lcd_image[DIMAGE_Y][DIMAGE_X][RGB_BYTES] __attribute__((section("lcd_image_buf")));					// 480x800x3 = 1,152,000
+uint8_t 	lcd_image[DIMAGE_Y][DIMAGE_X][RGB_BYTES] __attribute__((section(".bss.lcd_image_buf")));				// 480x800x3 = 1,152,000
 
