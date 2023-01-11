65aacef0c608810b2cb362fd134ef827473d4670
Jammu Kekkonen <jammu.kekkonen@alifsemi.com>
2023-01-11 11:35:41 +0200

    Take camera controller driver into use from cmsis pack

--- source/hal/source/components/image/source/ensemble/src/Driver_CPI.c
+++ source/hal/source/components/image/source/ensemble/src/Driver_CPI.c
@@ -1,132 +1,140 @@
 /* Copyright (C) 2022 Alif Semiconductor - All Rights Reserved.
  * Use, distribution and modification of this code is permitted under the
- * terms stated in the Alif Semiconductor Software License Agreement 
+ * terms stated in the Alif Semiconductor Software License Agreement
  *
- * You should have received a copy of the Alif Semiconductor Software 
- * License Agreement with this file. If not, please write to: 
+ * You should have received a copy of the Alif Semiconductor Software
+ * License Agreement with this file. If not, please write to:
  * contact@alifsemi.com, or visit: https://alifsemi.com/license
  *
  */
 
 /* System Includes */
-#include "RTE_Device.h"
 #include "RTE_Components.h"
+#include "RTE_Device.h"
 #include "global_map.h"
 
 /* Project Includes */
 #include "Driver_CPI.h"
-#include "arx3A0_camera_sensor.h"
-#include "arx3A0_camera_sensor_conf.h"
-#include "Driver_PINMUX_AND_PINPAD.h"
+#include "Driver_Camera_Controller.h"
 #include "Driver_GPIO.h"
-#include "mipi_csi2_host.h"
+#include "Driver_PINMUX_AND_PINPAD.h"
 
 #include "Driver_Common.h"
-#include "delay.h"
-#include "base_def.h"
 #include "image_processing.h"
 
-int32_t camera_init(uint8_t *buffer)
+#include <stdatomic.h>
+
+extern ARM_DRIVER_CAMERA_CONTROLLER Driver_CAMERA0;
+static const ARM_DRIVER_CAMERA_CONTROLLER* camera = &Driver_CAMERA0;
+
+static uint8_t* buf              = 0;
+static atomic_int image_received = 0;
+
+static void CameraEventHandler(uint32_t event)
 {
-	//////////////////////////////////////////////////////////////////////////////
-	//Camera initialization
-	//////////////////////////////////////////////////////////////////////////////
-	int retry = 5;
-	do {
-		arx3A0_Init(ARX3A0_CAMERA_RESOLUTION_560x560);
+    (void)event;
 
-		if (rx_phyconfig() == 0)
-			break;
-		retry--;
-	} while (retry > 0);
+    // only capture stopped event is configured to cause interrupt
+    image_received = 1;
+}
 
-	if (retry <= 0) {
-		return -1;
-	}
-	////////////////////////////////////////////////////////////////
-	//camera setup
-	////////////////////////////////////////////////////////////////
+static int32_t camera_hw_init()
+{
+    int32_t res = PINMUX_Config(PORT_NUMBER_3, PIN_NUMBER_8, PINMUX_ALTERNATE_FUNCTION_3);
+    if (res != ARM_DRIVER_OK) {
+        return res;
+    }
 
-	arx3A0_camera_init(ARX3A0_CAMERA_RESOLUTION_560x560);
-	arx3A0_Start();
+    /* Configure GPIO Pin : P3_9 as I3C_SCL_B */
+    res = PINMUX_Config(PORT_NUMBER_3, PIN_NUMBER_9, PINMUX_ALTERNATE_FUNCTION_4);
+    if (res != ARM_DRIVER_OK) {
+        return res;
+    }
 
-	////////////////////////////////////////////////////////////////
-	//3.3.1Start up DWC_mipi_csi2_host
-	////////////////////////////////////////////////////////////////
-	csi2_IPI_initialization();
-	csi2_IPI_enable();
+    /* i3c Pin-Pad */
 
-	// common settings for all camera
-	HW_REG_WORD(CAMERA0_BASE, CAM_CSI_CMCFG) = 3 << 4;             	// MIPI_CSI 16-bit RAW data
-	HW_REG_WORD(CAMERA0_BASE, CAM_INTR_ENA)  = 0x00000100;         	// enable irq: vsync
-	HW_REG_WORD(CAMERA0_BASE, CAM_FIFO_CTRL) = 0x00001707;         	// camera fifo water mark
+    /* Pin-Pad P3_8 as I3C_SDA_B
+     * Pad function: weak pull up(0x8) + read enable(0x01)
+     *               + Output drive strength 4mA(0x20)
+     */
+    res = PINPAD_Config(PORT_NUMBER_3,
+                        PIN_NUMBER_8,
+                        (PAD_FUNCTION_READ_ENABLE | PAD_FUNCTION_DRIVER_DISABLE_STATE_WITH_PULL_UP |
+                         PAD_FUNCTION_OUTPUT_DRIVE_STRENGTH_04_MILI_AMPS));
+    if (res != ARM_DRIVER_OK) {
+        return res;
+    }
 
-	// configure for common camera sensor
+    /* Pin-Pad P3_9 as I3C_SCL_B
+     * Pad function: weak pull up(0x8) + read enable(0x01)
+     *               + Output drive strength 4mA(0x20)
+     */
+    res = PINPAD_Config(PORT_NUMBER_3,
+                        PIN_NUMBER_9,
+                        (PAD_FUNCTION_READ_ENABLE | PAD_FUNCTION_DRIVER_DISABLE_STATE_WITH_PULL_UP |
+                         PAD_FUNCTION_OUTPUT_DRIVE_STRENGTH_04_MILI_AMPS));
+    if (res != ARM_DRIVER_OK) {
+        return res;
+    }
 
-#if (PIXEL_BYTES == 2)
-	HW_REG_WORD(CAMERA0_BASE, CAM_CFG) = (1<<28) | (1<<4);   		// select 16-bit data mode, select CSI
-#else
-	HW_REG_WORD(CAMERA0_BASE, CAM_CFG) = (1<<4);
-#endif
-	HW_REG_WORD(CAMERA0_BASE, CAM_VIDEO_FCFG) = (CIMAGE_X-1)<<16 | (1<<15) | (CIMAGE_Y-1);      // row, data
-	HW_REG_WORD(CAMERA0_BASE, CAM_VIDEO_HCFG) = 0<<16 | (15-2);     // hfp, hbp
+    res = PINMUX_Config(PORT_NUMBER_2, PIN_NUMBER_7, PINMUX_ALTERNATE_FUNCTION_6);
+    if (res != ARM_DRIVER_OK) {
+        return res;
+    }
 
-	// image buffer address, crossing 4KB boundary
-	HW_REG_WORD(CAMERA0_BASE, CAM_FRAME_ADDR) = (uint32_t)buffer;
-	sleep_or_wait_msec(1);
-	// camera start sequence
-	HW_REG_WORD(CAMERA0_BASE, CAM_CTRL) = 0x00000100;         // camera soft reset
-	HW_REG_WORD(CAMERA0_BASE, CAM_CTRL) = 0x00000000;         // clear camera soft reset
+    return res;
+}
 
-	HW_REG_WORD(CAMERA0_BASE, CAM_INTR_ENA) = 1;              // enable STOP interrupt
-	NVIC_EnableIRQ (CAMERA0_IRQ);
+int32_t camera_init(uint8_t* buffer)
+{
+    // Pin configurations
+    int32_t res = camera_hw_init();
+    if (res != ARM_DRIVER_OK) {
+        return res;
+    }
+    //////////////////////////////////////////////////////////////////////////////
+    // Camera initialization
+    //////////////////////////////////////////////////////////////////////////////
+    res = camera->Initialize(CAMERA_RESOLUTION_560x560, CameraEventHandler);
 
-	return 0;
+    if (res != ARM_DRIVER_OK) {
+        return res;
+    }
+
+    res = camera->PowerControl(ARM_POWER_FULL);
+    if (res != ARM_DRIVER_OK) {
+        return res;
+    }
+
+    res = camera->Control(CAMERA_SENSOR_CONFIG, CAMERA_RESOLUTION_560x560);
+    if (res != ARM_DRIVER_OK) {
+        return res;
+    }
+
+    res = camera->Control(CAMERA_EVENTS_CONFIG, ARM_CAMERA_CONTROLLER_EVENT_CAMERA_CAPTURE_STOPPED);
+    if (res != ARM_DRIVER_OK) {
+        return res;
+    }
+
+    buf = buffer;
+
+    return res;
 }
 
 void camera_start(uint32_t mode)
 {
-	if (mode == CAMERA_MODE_SNAPSHOT) {
-		HW_REG_WORD(CAMERA0_BASE, CAM_CTRL) = 0x00000100;         // camera soft reset
-		sleep_or_wait_usec(50);
-		HW_REG_WORD(CAMERA0_BASE, CAM_CTRL) = 0x00000000;         // clear camera soft reset
-	}
-	HW_REG_WORD(CAMERA0_BASE, CAM_CTRL) = mode;
-}
-
-// Don't reeally need to do anything apart from clear the interrupts;
-// we're just generating the IRQ to wake from WFE.
-void CAMERA0_IRQHandler(void)
-{
-	// Read which interrupts are pending
-	uint32_t interrupts = HW_REG_WORD(CAMERA0_BASE, CAM_INTR);
-
-	// Write back to acknowledge clear the ones we saw
-	HW_REG_WORD(CAMERA0_BASE, CAM_INTR) = interrupts;
-
-	// Read back to ensure that write reached the camera so we do
-	// not return before it deasserts the IRQ.
-	HW_REG_WORD(CAMERA0_BASE, CAM_INTR);
-}
-
-int32_t camera_vsync(uint32_t timeout_ms)
-{
-	int camera_irq;
-
-	do {
-		camera_irq = HW_REG_WORD(CAMERA0_BASE, CAM_INTR);
-		camera_irq &= 1 << 8;                              	// mask all except vsync irq bit
-	} while (camera_irq == 0);
-
-	HW_REG_WORD(CAMERA0_BASE,CAM_INTR) = 1 << 8;			// Clear IRQ flag
-
-	return ARM_DRIVER_OK;
+    image_received = 0;
+    if (mode == CAMERA_MODE_SNAPSHOT) {
+        camera->CaptureFrame(buf);
+    } else {
+        camera->CaptureVideo(buf);
+    }
 }
 
 int32_t camera_wait(uint32_t timeout_ms)
 {
-	while(HW_REG_WORD(CAMERA0_BASE, CAM_CTRL) & 0x2) {
-		__WFE();
-	};
-	return ARM_DRIVER_OK;
+    while (!image_received) {
+        __WFE();
+    };
+    return ARM_DRIVER_OK;
 }
