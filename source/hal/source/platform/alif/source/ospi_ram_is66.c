79fede1a94f632fb55b67b5c28a381d836d1127a
Kevin Bracey <kevin.bracey@alifsemi.com>
2025-01-07 14:19:00 +0200

    Complete IS66 HyperRAM driver

--- source/hal/source/platform/alif/source/ospi_ram_is66.c
+++ source/hal/source/platform/alif/source/ospi_ram_is66.c
@@ -21,6 +21,7 @@
 
 #include "board.h"
 #include "pinconf.h"
+#include "Driver_GPIO.h"
 #include "Driver_OSPI.h"
 #include "ospi.h"
 #include "ospi_hyperram_xip.h"
@@ -40,16 +41,26 @@
 //#define DRIVER_OSPI_BUS_SPEED 500000 // 20MHz FPGA
 #define DDR_DRIVE_EDGE 1
 #define RXDS_DELAY 11
+//#define SET_VARIABLE_LATENCY
 #define ISSI_WAIT_CYCLES    6
+
+#define OSPI_RESET_PORT     BOARD_OSPI_RAM_RESET_GPIO_PORT
+#define OSPI_RESET_PIN      BOARD_OSPI_RAM_RESET_PIN_NO
+
+extern ARM_DRIVER_GPIO ARM_Driver_GPIO_(OSPI_RESET_PORT);
+static ARM_DRIVER_GPIO* const GPIODrv = &ARM_Driver_GPIO_(OSPI_RESET_PORT);
+
+static void hyperram_init(void);
 
 static const ospi_hyperram_xip_config issi_config = {
     .instance       = DRIVER_OSPI_NUM,
     .bus_speed      = DRIVER_OSPI_BUS_SPEED,
-    .hyperram_init  = NULL, /* No special initialization needed by the hyperram device */
+    .hyperram_init  = hyperram_init,
     .ddr_drive_edge = DDR_DRIVE_EDGE,
     .rxds_delay     = RXDS_DELAY,
     .wait_cycles    = ISSI_WAIT_CYCLES,
     .slave_select   = 0,
+    .bus_width      = 8
 };
 
 extern ARM_DRIVER_OSPI ARM_Driver_OSPI_(DRIVER_OSPI_NUM);
@@ -66,236 +77,93 @@ static AES_Type * const aes = (AES_Type *)AES1_BASE;
 
 #define WAIT_TIMEOUT                            128
 
-#if 0
-static int reg_read_wait_cycles = RAM_DEVICE_INIT_REG_READ_WAIT_CYCLES;
-static atomic_uint_least32_t event_flag;
-static void spi_callback_event(uint32_t event)
+static int32_t ospi_toggle_reset(void)
 {
-    event_flag = event;
+    int32_t ret = GPIODrv->Initialize(OSPI_RESET_PIN, NULL);
+    if (ret != ARM_DRIVER_OK) { return ret; }
+
+    ret = GPIODrv->PowerControl(OSPI_RESET_PIN, ARM_POWER_FULL);
+    if (ret != ARM_DRIVER_OK) { return ret; }
+
+    ret = GPIODrv->SetDirection(OSPI_RESET_PIN, GPIO_PIN_DIRECTION_OUTPUT);
+    if (ret != ARM_DRIVER_OK) { return ret; }
+
+    ret = GPIODrv->SetValue(OSPI_RESET_PIN, GPIO_PIN_OUTPUT_STATE_LOW);
+    if (ret != ARM_DRIVER_OK) { return ret; }
+
+    ret = GPIODrv->SetValue(OSPI_RESET_PIN, GPIO_PIN_OUTPUT_STATE_HIGH);
+    if (ret != ARM_DRIVER_OK) { return ret; }
+
+    return ret;
 }
 
-static int32_t wait_for_completion(void)
+#ifdef SET_VARIABLE_LATENCY
+static void hyperram_write_register(uint32_t addr, uint16_t value)
 {
-    uint32_t start_time = Get_SysTick_Count();
-    while (event_flag == 0 && Get_SysTick_Count() - start_time < WAIT_TIMEOUT) {
-        __WFE();
+    ospi_disable(ospi);
+    ospi->OSPI_CTRLR0 = SPI_CTRLR0_SSI_IS_MST_MASTER
+                      | SPI_CTRLR0_SPI_HYPERBUS_ENABLE
+                      | SPI_CTRLR0_SPI_FRF_OCTAL
+                      | SPI_CTRLR0_TMOD_SEND_ONLY
+                      | SPI_CTRLR0_DFS_16bit;
+
+    ospi->OSPI_SPI_CTRLR0 = (0 << SPI_CTRLR0_SPI_RXDS_SIG_EN_OFFSET)
+                          | (0 << SPI_CTRLR0_SPI_DM_EN_OFFSET)
+                          | (SPI_CTRLR0_SPI_RXDS_ENABLE << SPI_CTRLR0_SPI_RXDS_EN_OFFSET)
+                          | (1 << SPI_CTRLR0_SPI_DDR_EN_OFFSET)
+                          | (0 << SPI_CTRLR0_WAIT_CYCLES_OFFSET)
+                          | (SPI_CTRLR0_INST_L_0bit << SPI_CTRLR0_INST_L_OFFSET)
+                          | (12 << SPI_CTRLR0_ADDR_L_OFFSET)
+                          | (SPI_TRANS_TYPE_FRF_DEFINED << SPI_CTRLR0_TRANS_TYPE_OFFSET);
+
+    ospi->OSPI_TXFTLR = 2 << SPI_TXFTLR_TXFTHR_SHIFT;
+
+    ospi_enable(ospi);
+
+    ospi->OSPI_DR0 = 0x60000000 | (addr >> 16);
+    ospi->OSPI_DR0 = addr & 0xFFFF;
+    ospi->OSPI_DR0 = value;
+
+    while (!(ospi->OSPI_SR & SPI_SR_TX_FIFO_EMPTY)) {
+
     }
-    int32_t flag = atomic_exchange(&event_flag, 0);
-    if ((flag & (ARM_OSPI_EVENT_TRANSFER_COMPLETE | ARM_OSPI_EVENT_DATA_LOST | ARM_OSPI_EVENT_MODE_FAULT)) == ARM_OSPI_EVENT_TRANSFER_COMPLETE) {
-        return ARM_DRIVER_OK;
-    } else if (flag == 0) {
-        return ARM_DRIVER_ERROR_TIMEOUT;
-    } else {
-        return ARM_DRIVER_ERROR;
-    }
-}
-
-static uint32_t control_flags = ARM_OSPI_DATA_BITS(16) |
-        ARM_OSPI_SS_MASTER_HW_OUTPUT | ARM_OSPI_CPOL0_CPHA0 | ARM_OSPI_MSB_LSB;
-
-static int32_t do_global_reset(void)
-{
-    int32_t ret;
-    uint32_t cmd[1];
-
-    ret = ptrOSPI->Control(ARM_OSPI_SET_ADDR_LENGTH_WAIT_CYCLE | control_flags, (ARM_OSPI_ADDR_LENGTH_0_BITS << ARM_OSPI_ADDR_LENGTH_POS) | (RAM_DEVICE_RESET_WAIT_CYCLES << ARM_OSPI_WAIT_CYCLE_POS));
-    if (ret != ARM_DRIVER_OK)
-    {
-        printf_err("do_global_reset OSPI control len/wait failed\n");
-        return ret;
-    }
-
-    event_flag = 0;
-    cmd[0] = CMD_GLOBAL_RESET;
-    ret = ptrOSPI->Send(&cmd[0], 1);
-    if (ret != ARM_DRIVER_OK)
-    {
-        printf_err("do_global_reset OSPI send failed\n");
-        return ret;
-    }
-    ret = wait_for_completion();
-    if (ret != ARM_DRIVER_OK)
-    {
-        printf_err("do_global_reset wait failed\n");
-        return ret;
-    }
-
-    return ARM_DRIVER_OK;
-}
-
-
-static int32_t reg_read(uint8_t reg, uint8_t *value)
-{
-    int32_t ret;
-    uint32_t cmd[2];
-    uint16_t data[2];
-
-    ret = ptrOSPI->Control(ARM_OSPI_SET_ADDR_LENGTH_WAIT_CYCLE | control_flags, (ARM_OSPI_ADDR_LENGTH_32_BITS << ARM_OSPI_ADDR_LENGTH_POS) | (reg_read_wait_cycles << ARM_OSPI_WAIT_CYCLE_POS));
-    if (ret != ARM_DRIVER_OK)
-    {
-        printf_err("reg_read OSPI control len/wait failed\n");
-        return ret;
-    }
-
-    event_flag = 0;
-    cmd[0] = CMD_MODE_REGISTER_READ;
-    cmd[1] = reg;
-    ret = ptrOSPI->Transfer(cmd, data, 1);
-    if (ret != ARM_DRIVER_OK)
-    {
-        printf_err("reg_read OSPI transfer failed\n");
-        return ret;
-    }
-    ret = wait_for_completion();
-    if (ret != ARM_DRIVER_OK)
-    {
-        printf_err("reg_read wait failed\n");
-        return ret;
-    }
-
-    printf("data = %" PRIX16 "\n", data[0]);
-    *value = data[0] >> 8;
-    return ARM_DRIVER_OK;
-}
-
-static int32_t reg_write(uint8_t reg, uint8_t value)
-{
-    int32_t ret;
-    uint32_t cmd[3];
-
-    ret = ptrOSPI->Control(ARM_OSPI_SET_ADDR_LENGTH_WAIT_CYCLE | control_flags, (ARM_OSPI_ADDR_LENGTH_32_BITS << ARM_OSPI_ADDR_LENGTH_POS) | (RAM_DEVICE_REG_WRITE_WAIT_CYCLES << ARM_OSPI_WAIT_CYCLE_POS));
-    if (ret != ARM_DRIVER_OK)
-    {
-        printf_err("reg_write OSPI control len/wait failed\n");
-        return ret;
-    }
-
-    event_flag = 0;
-    cmd[0] = CMD_MODE_REGISTER_WRITE;
-    cmd[1] = reg;
-    cmd[2] = value << 8;
-    ret = ptrOSPI->Send(cmd, 3);
-    if (ret != ARM_DRIVER_OK)
-    {
-        printf_err("reg_write OSPI send failed\n");
-        return ret;
-    }
-    ret = wait_for_completion();
-    if (ret != ARM_DRIVER_OK)
-    {
-        printf_err("reg_write wait failed\n");
-        return ret;
-    }
-
-    return ARM_DRIVER_OK;
 }
 #endif
+
+static void hyperram_init(void)
+{
+    int32_t ret = ospi_toggle_reset();
+    if (ret != ARM_DRIVER_OK) {
+        printf_err("OSPI reset failed\n");
+    }
+
+#ifdef SET_VARIABLE_LATENCY
+    // 32M parts default to fixed latency - this could poke it to variable
+    // But 64M (stacked) parts don't support variable. This seems to be ignored,
+    // but manual suggests we shouldn't do it.
+    ospi_disable(ospi);
+
+    ospi->OSPI_SER = 1;
+
+#if ISSI_WAIT_CYCLES < 3 || ISSI_WAIT_CYCLES > 8
+#error "Invalid wait cycles"
+#endif
+
+    uint16_t cr = (1 << 15) // Normal operation
+                | (0 << 12) // 34 ohm output drive strength (default)
+                | (((ISSI_WAIT_CYCLES - 5) & 0xF) << 4) // initial latency
+                | (0 << 3) // variable latency
+                | (1 << 2) // standard wrapped operation
+                | (3 << 0); // 32-byte wrap
+
+    hyperram_write_register(0x01000000, cr);
+#endif
+}
 
 int32_t ospi_ram_init(void)
 {
-#if 0 // Only needed if we want to do some speed tuning setup
-
-    event_flag = 0;
-
-    int32_t ret;
-    ret = ptrOSPI->Initialize(spi_callback_event);
-    if (ret != ARM_DRIVER_OK)
-    {
-        printf_err("OSPI driver init failed\n");
-        return ret;
-    }
-
-#if DRIVER_OSPI_NUM == 1
-    // Current pack doesn't enable OSPI1...
-    CLKCTL_PER_SLV->OSPI_CTRL |= 1 << 1;
-#endif
-
-    ret = ptrOSPI->PowerControl(ARM_POWER_FULL);
-    if (ret != ARM_DRIVER_OK)
-    {
-        printf_err("OSPI power control failed\n");
-        return ret;
-    }
-
-    ret = ptrOSPI->Control(ARM_OSPI_MODE_MASTER | control_flags, DRIVER_OSPI_BUS_SPEED);
-    if (ret != ARM_DRIVER_OK)
-    {
-        printf_err("OSPI control mode failed\n");
-        return ret;
-    }
-
-    ret = ptrOSPI->Control(ARM_OSPI_SET_FRAME_FORMAT | control_flags, ARM_OSPI_FRF_OCTAL);
-    if (ret != ARM_DRIVER_OK)
-    {
-        printf_err("OSPI control frame format failed\n");
-        return ret;
-    }
-
-    ret = ptrOSPI->Control(ARM_OSPI_SET_DDR_MODE | control_flags, ARM_OSPI_DDR_ENABLE);
-    if (ret != ARM_DRIVER_OK)
-    {
-        printf_err("OSPI control DDR mode failed\n");
-        return ret;
-    }
-
-    bool slave_good[DRIVER_RAM_DEVICES] = { 0 };
-    for (int slave = 0; slave < DRIVER_RAM_DEVICES; slave++) {
-        printf("Attempting APS512XX RAM initialisation (device %d)\n", slave);
-
-        // OSPI driver doesn't provide dynamic chip select control
-        ospi_control_ss(ospi, slave, SPI_SS_STATE_ENABLE);
-
-        do_global_reset();
-
-        reg_read_wait_cycles = RAM_DEVICE_INIT_REG_READ_WAIT_CYCLES;
-
-       /* for (int delay = 0; delay < 15; delay++) {
-            aes_set_rxds_delay(aes, delay);*/
-
-            uint8_t value;
-            ret = reg_read(2, &value);
-            if (ret != ARM_DRIVER_OK)
-            {
-                printf("RAM %d read error\n", slave);
-                return ret;
-            }
-
-            //printf("delay %2d: MR2 = %02" PRIX8 "\n", delay, value);
-       // }
-        printf("MR2 = %02" PRIX8 "\n", value);
-
-        // MR0: Set minimum variable read latency (3 cycles)
-        ret = reg_write(0, 0);
-        if (ret != ARM_DRIVER_OK)
-        {
-            return ret;
-        }
-
-        reg_read_wait_cycles = RAM_DEVICE_REG_READ_WAIT_CYCLES;
-
-        // MR4: Minimum write latency (3 cycles), auto slow refresh
-        ret = reg_write(4, 0x18);
-        if (ret != ARM_DRIVER_OK)
-        {
-            return ret;
-        }
-
-        // MR8: x8, 32-byte wrap, no row cross
-        ret = reg_write(8, 0x03);
-        if (ret != ARM_DRIVER_OK)
-        {
-            return ret;
-        }
-
-        ospi_control_ss(ospi, slave, SPI_SS_STATE_DISABLE);
-        printf("APS512XX RAM %d apparently okay\n", slave);
-        slave_good[slave] = true;
-    }
-
-#else
 #ifdef DEVICE_FEATURE_OSPI_CTRL_CLK_ENABLE
     CLKCTL_PER_SLV->OSPI_CTRL |= 1 << DRIVER_OSPI_NUM;
-#endif
 #endif
 
     if (ospi_hyperram_xip_init(&issi_config) < 0)
