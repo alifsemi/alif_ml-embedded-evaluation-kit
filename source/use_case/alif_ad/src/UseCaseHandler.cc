87c89d25fe0dc2b3cc628597b039e6cdbdf38077
Teppo Jarvelin <teppo.jarvelin@alifsemi.com>
2025-12-10 10:34:01 +0200

    alif_ad working with gcc

--- source/use_case/alif_ad/src/UseCaseHandler.cc
+++ source/use_case/alif_ad/src/UseCaseHandler.cc
@@ -35,10 +35,10 @@
 using arm::app::Profiler;
 using arm::app::ClassificationResult;
 using arm::app::ApplicationContext;
-using arm::app::Model;
+using arm::app::fwk::iface::Model;
 using arm::app::AdPreProcess;
 using arm::app::AdPostProcess;
-using arm::app::AdModel;
+using arm::app::fwk::tflm::AdModel;
 
 
 #define AUDIO_SAMPLES 33280
@@ -47,6 +47,7 @@ using arm::app::AdModel;
 #define RESULTS_MEMORY 8
 
 static int16_t audio_inf[AUDIO_SAMPLES + AUDIO_STRIDE];
+
 
 namespace alif {
 namespace app {
@@ -71,9 +72,6 @@ using namespace arm::app::ad;
     /* Anomaly Detection inference handler */
     bool ClassifyVibrationHandler(ApplicationContext& ctx)
     {
-        constexpr uint32_t dataPsnTxtInfStartX = 20;
-        constexpr uint32_t dataPsnTxtInfStartY = 40;
-
         auto& model = ctx.Get<Model&>("model");
         if (!model.IsInited()) {
             printf_err("Model is not initialised! Terminating processing.\n");
@@ -89,19 +87,20 @@ using namespace arm::app::ad;
         const auto trainingMean = ctx.Get<float>("trainingMean");
         const auto machineGain = ctx.Get<float>("machineGain");
 
-        TfLiteTensor* outputTensor = model.GetOutputTensor(0);
-        TfLiteTensor* inputTensor = model.GetInputTensor(0);
+        auto outputTensor = model.GetOutputTensor(0);
+        auto inputTensor  = model.GetInputTensor(0);
 
-        if (!inputTensor->dims) {
+        if (inputTensor->Shape().empty()) {
             printf_err("Invalid input tensor dims\n");
             return false;
         }
 
-        AdPreProcess preProcess{
-            inputTensor,
-            melSpecFrameLength,
-            melSpecFrameStride,
-            trainingMean};
+        AdPreProcess preProcess{inputTensor,
+                        AdModel::ms_inputRowsIdx,
+                        AdModel::ms_inputColsIdx,
+                        melSpecFrameLength,
+                        melSpecFrameStride,
+                        trainingMean};
 
         AdPostProcess postProcess{outputTensor};
 
