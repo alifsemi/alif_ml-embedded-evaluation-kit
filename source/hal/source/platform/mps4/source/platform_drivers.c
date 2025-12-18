9e9957c6dc9b3cfb95eb63b16e4beecbfe7ab756
Kshitij Sisodia <kshitij.sisodia@arm.com>
2025-12-18 15:32:14 +0000

    MLECO-6814: Add visuals for Conformer example
    Add audio and MEL spectrogram plot for speech recognition
    example using Conformer model (ExecuTorch based).
    
    Include a fix in HAL to render RGB888 format correctly for
    MPS4 FVPs.
    
    Signed-off-by: Kshitij Sisodia <kshitij.sisodia@arm.com>
    Change-Id: I256539b2d314039a534a59c81c4cda897e76d039
    Signed-off-by: Kshitij Sisodia <kshitij.sisodia@arm.com>

--- source/hal/source/platform/mps4/source/platform_drivers.c
+++ source/hal/source/platform/mps4/source/platform_drivers.c
@@ -283,16 +283,15 @@ static int platform_hdlcd_init(void)
     }
     trace("HDLCD base address set\n");
 
-    const struct hdlcd_pixel_cfg_t hdlcd_pixel_cfg_rgb888 = {
-        .red.default_color = 0x00,
-        .red.bit_size = 0x8,
-        .red.offset = 0x10,
-        .green.default_color = 0x00,
-        .green.bit_size = 0x8,
-        .green.offset = 0x8,
-        .blue.default_color = 0x00,
-        .blue.bit_size = 0x8,
-        .blue.offset = 0x0};
+    const struct hdlcd_pixel_cfg_t hdlcd_pixel_cfg_rgb888 = {.red.default_color   = 0x00,
+                                                             .red.bit_size        = 0x8,
+                                                             .red.offset          = 0x00,
+                                                             .green.default_color = 0x00,
+                                                             .green.bit_size      = 0x8,
+                                                             .green.offset        = 0x8,
+                                                             .blue.default_color  = 0x00,
+                                                             .blue.bit_size       = 0x8,
+                                                             .blue.offset         = 0x10};
 
     hdlcd_err = hdlcd_pixel_config(hdlcd_dev, &hdlcd_pixel_cfg_rgb888);
     if (hdlcd_err != HDLCD_ERR_NONE) {
